c3ebc8037cdc03bbb76d8063bafa7cb49cd21da9
Steven <StevenPF@gmail.com>
2014-02-03 14:26:27 +0100

    Création du dossier Opera15+

---
+++ Opera15+/pf-plus/data/background.js
@@ -0,0 +1,60 @@
+/**
+* Background JS for getting options
+*/
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getStorage") {
+		if(request.key == "all") {
+			var values = {
+				// Chemins de l'extension
+				"dataPath": chrome.extension.getURL("data") + "/",
+				"sharedPath": chrome.extension.getURL("data") + "/shared/",
+				
+				// Options de l'extension
+				"displayNewLogo": localStorage.getItem('displayNewLogo') === "true" ? true : false,
+				"displayUniqueTheme": localStorage.getItem('displayUniqueTheme') === "true" ? true : false,
+				"selectedUniqueTheme": localStorage.getItem('selectedUniqueTheme'),
+				"removeSignatures": localStorage.getItem('removeSignatures') === "true" ? true : false,
+				"fixMaxWidthImg": localStorage.getItem('fixMaxWidthImg') === "true" ? true : false,
+				"addTopButtons": localStorage.getItem('addTopButtons') === "true" ? true : false,
+				"fastQuote": localStorage.getItem('fastQuote') === "true" ? true : false,
+				"displayYoutube": localStorage.getItem('displayYoutube') === "true" ? true : false,
+				"displayDailymotion": localStorage.getItem('displayDailymotion') === "true" ? true : false,
+				"displayTwitter": localStorage.getItem('displayTwitter') === "true" ? true : false,
+				"lazyLoadingOnImg": localStorage.getItem('lazyLoadingOnImg') === "true" ? true : false,
+				
+				// Styles par constructeur
+				"themes": {
+					"sony": {
+						"forum": "/css/forum_pf.css",
+						"base":  "/css/base_color_pf.css",
+						"style": "/include/the_style1.php?color_key=FFFFFF/f9f9f9/000080/92c5c5/307099/FFFFFF/FFFFFF/000000/000000/000000/2b68a9/FFFFFF/F8F8F8/FFFFFF/F8F8F8/f1e9e9/FFFFFF/F8F8F8/000000/000000/0000FF/EEEEFF/DDDDEE/000000/FFEEEE/000000/FFFFFF/FF0000/FFFFFF/0/0/static/NULL/&abs_img_path=%40data%40www%40vhosts%40httpdocs%40www%40static%40&hide_bg_onglet=0&v=11102781422"
+					},
+					"microsoft": {
+						"forum": "/css/forum_xf.css",
+						"base":  "/css/base_color_xf.css",
+						"style": "/include/the_style1.php?color_key=FFFFFF/f9f9f9/000080/92c5c5/CCCCCC/FFFFFF/FFFFFF/000000/000000/000000/000066/FFFFFF/F8F8F8/FFFFFF/F8F8F8/f1e9e9/FFFFFF/F8F8F8/000000/000000/0000FF/EEEEFF/DDDDEE/000000/FFEEEE/000000/FFFFFF/FF0000/FFFFFF/0/0/static/NULL/&abs_img_path=%40data%40www%40vhosts%40httpdocs%40www%40static%40&hide_bg_onglet=0&v=11102781422"
+					},
+					"nintendo": {
+						"forum": "/css/forum_wf.css",
+						"base":  "/css/base_color_wf.css",
+						"style": "/include/the_style1.php?color_key=FFFFFF/F0F0F0/000080/C2C3F4/D5E6E1/000080/000080/000000/000080/000000/000066/F7F7F7/DEDFDF/F7F7F7/DEDFDF/C0C0C0/FFFFFF/FFFFFF/000000/000000/0000FF/EEEEFF/DDDDEE/000000/FFEEEE/000000/FFFFFF/FF0000/FFFFFF/0/0/static/NULL/&abs_img_path=%40data%40www%40vhosts%40httpdocs%40www%40static%40&hide_bg_onglet=0&v=11102781422"
+					},
+					"pc": {
+						"forum": "/css/forum_pca.css",
+						"base":  "/css/base_color_pca.css",
+						"style": "/include/the_style1.php?color_key=FFFFFF/F0F0F0/000080/C2C3F4/D5E6E1/000080/000080/000000/000080/000000/000066/F7F7F7/DEDFDF/F7F7F7/DEDFDF/C0C0C0/FFFFFF/FFFFFF/000000/000000/0000FF/EEEEFF/DDDDEE/000000/FFEEEE/000000/FFFFFF/FF0000/FFFFFF/0/0/static/NULL/&abs_img_path=%40data%40www%40vhosts%40httpdocs%40www%40static%40&hide_bg_onglet=0&v=11102781422"
+					}
+				}
+			};
+			
+			sendResponse({data: values});
+		}
+		else {
+			sendResponse({data: localStorage[request.key]});
+		}
+	}
+    else {
+		sendResponse({}); // snub them.
+	}
+});
---
+++ Opera15+/pf-plus/data/shared/common.js
@@ -0,0 +1,178 @@
+function applyPfPlus(values) {
+	// Changement du logo
+	if (values.displayNewLogo) {
+		$('a.logo').css({
+			"background-image": "url(" + values.sharedPath + "img/logo.png)",
+			"width": "210px",
+			"left": "10px"
+		});
+	}
+	
+	// Changement du thème
+	if (values.displayUniqueTheme) {
+		var $link1 = $('<link type="text/css" rel="stylesheet" href="' + values.themes[values.selectedUniqueTheme].style + '" />');
+		var $link2 = $('<link type="text/css" rel="stylesheet" href="' + values.themes[values.selectedUniqueTheme].forum + '" />');
+		var $link3 = $('<link type="text/css" rel="stylesheet" href="' + values.themes[values.selectedUniqueTheme].base + '" />');
+		$(document.head).append($link1).append($link2).append($link3);
+		$(".bg-holder").css("background", "").attr("class", "level-1");
+	}
+
+	// Désactivation des signatures
+	if (values.removeSignatures) {
+		$(".signature").remove();
+	}
+	
+	// Redimensionnement des images
+	if (values.fixMaxWidthImg) {
+		$(".messagetable").css({
+			"table-layout": "fixed"
+		});
+
+		$(".messCase2 img").css({
+			"max-width": "100%",
+			"width": "auto",
+			"height": "auto"
+		});
+	}
+	
+	// Ajout du bouton Haut de page
+	if (values.addTopButtons) {
+		$(".messCase2 .toolbar").each(function(index) {
+			var $this = $(this).css({
+				"overflow": "hidden"
+			});
+			$this.append('<div class="right"><a href="#haut" style="color:rgb(43, 104, 169); font-weight: bold;">Haut de page</a></div>');
+			$this.children(".spacer").remove();
+		});
+	}
+	
+	// Ajout du clic droit pour quoter un message
+	if (values.fastQuote /* || Autres contextMenu */) {
+		// Code générique ContextMenu
+		$('body').append('<div class="pf-plus-context-menu"></div>');
+		var $context = $('.pf-plus-context-menu');
+		
+		// Spécifique FastQuote
+		if (values.fastQuote && $('.messCase2:first').length > 0) {
+			var $textarea = $("textarea.reponserapide");			
+			if ($textarea.length) {
+				$(document).on("click", function() {
+					$context.html("").hide();
+				}).on("contextmenu", function(documentEvent) {
+					$context.html("").hide();
+					
+					var selection = document.getSelection();
+					var text = selection.toString();
+					var isInComment = $(selection.anchorNode.parentNode).parents('.messCase2').length > 0;
+					
+					if (text.length && isInComment) {
+							$context.html(
+								$('<a href="#">').html("Citer le texte s&eacute;lectionn&eacute;").on("click", function(linkEvent) {
+									$textarea.trigger("focus").append('[quote]' + text + '[/quote]');
+									window.scrollTo(0, $textarea.offset().top);
+									$context.hide();
+									return false;
+								}
+							)).on("show", function(ev, oEv){
+								var $element = $(this);
+								$element.show().css({
+									"left": "0px",
+									"right": ""
+								});
+								
+								$element.css({
+									"top": (oEv.pageY + 10) + "px",
+									"left": (oEv.pageX + 10) + "px"
+								});
+								
+								var w = $element.outerWidth();
+								if (oEv.pageX + 20 + w > window.innerWidth) {
+									$element.css({
+										"right": "10px",
+										"left": ""
+									});
+								}
+							}).trigger("show", documentEvent);
+							return false;
+					}
+				});
+			}
+		}
+	}
+	
+	// Chargement des vidéos
+	// Youtube
+	if (values.displayYoutube) {
+		$("a[href*='youtu.be'], a[href*='youtube.com']").each(function(index) {
+			var $a = $(this);
+			
+			var idVideo = null;
+			if ($a.attr('uri:domain') == 'youtube.com') {
+				idVideo = URI.parseQuery($a.attr('uri:query')).v;
+			} else {
+				idVideo = $a.attr('uri:filename');
+			}
+			
+			if (idVideo) {
+				$a.replaceWith('<iframe width="560" height="315" src="//www.youtube.com/embed/' + idVideo + '" frameborder="0" allowfullscreen></iframe>');
+			}
+		});
+	}
+		
+	// Dailymotion
+	if (values.displayDailymotion) {
+		$("a[href*='dai.ly'], a[href*='dailymotion.com/video/']").each(function(index) {
+			var $a = $(this);
+			
+			var idVideo = $a.attr('uri:filename').split('_')[0];
+			
+			if (idVideo) {
+				$a.replaceWith('<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/' + idVideo + '" allowfullscreen></iframe>');
+			}
+		});
+	}
+	
+	// Chargement des tweets
+	if (values.displayTwitter) {
+		$("a[href*='twitter.com']").each(function( listIndex ) {
+			var $tweet = $(this);
+			var href = $tweet.attr('href');
+			
+			if (href.indexOf('status') > 0) {// Lien au format classique https://twitter.com/jack/status/20
+				$tweet.replaceWith('<iframe border=0 frameborder=0 height=250 width=550 src="https://twitframe.com/show?url=' + href + '"></iframe><br />');
+				return;
+			}
+		});
+
+		/* find all iframes with ids starting with "tweet_" */
+		$("iframe[id^='tweet_']").load(function() {
+			this.contentWindow.postMessage({ element: this.id, query: "height" }, "https://twitframe.com");
+		});
+
+		/* listen for the return message once the tweet has been loaded */
+		$(window).bind("message", function(e) {
+			var oe = e.originalEvent;
+			if (oe.origin != "https://twitframe.com") {
+				return;
+			}
+			
+			if (oe.data.height && oe.data.element.match(/^tweet_/)) {
+				$("#" + oe.data.element).css("height", parseInt(oe.data.height) + "px");
+			}
+		});
+	}
+	
+	// Chargement des images
+	if (values.lazyLoadingOnImg) {
+		$("img:not(img[src^='/static/'])").each(function(index) {
+			var $img = $(this);
+			var src = $img.attr('src');
+			$img.attr("class", "lazy").attr("data-original", src).attr("src", values.sharedPath + "img/spacer.gif");
+		});
+		
+		$(".lazy").lazyload({
+			threshold : 200,
+			effect: "fadeIn"
+		});
+	}
+}

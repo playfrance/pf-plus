40cb4ae52436744c9cf73cd1c3b90adc6125f12f
StevenHanquez <stevenpf@gmail.com>
2014-02-06 12:58:34 +0100

    Correction vidéo youTube dans signature Correction citation Amélioration réponse rapide avec aperçu Signed-off-by: StevenHanquez <stevenpf@gmail.com>

--- Chrome/pf-plus/data/shared/common.js
+++ Chrome/pf-plus/data/shared/common.js
@@ -66,34 +66,50 @@ function applyPfPlus(values) {
 					var isInComment = $(selection.anchorNode.parentNode).parents('.messCase2').length > 0;
 					
 					if (text.length && isInComment) {
-							$context.html(
-								$('<a href="#">').html("Citer le texte s&eacute;lectionn&eacute;").on("click", function(linkEvent) {
-									$textarea.trigger("focus").append('[quote]' + text + '[/quote]');
-									window.scrollTo(0, $textarea.offset().top);
-									$context.hide();
-									return false;
-								}
-							)).on("show", function(ev, oEv){
-								var $element = $(this);
-								$element.show().css({
-									"left": "0px",
-									"right": ""
-								});
-								
+						// Récupération de l'auteur du message cité
+						var $parentRoot = $(selection.anchorNode.parentNode).parents('.message');
+						var $topicParams = $parentRoot.find('a[href*="/citer-"]:first').attr("href").split('/citer-')[1].split('.')[0].split('-');
+						var $authorId = $parentRoot.find('a[href*="/profil-"]:first').attr("href").split('/profil-')[1].split('.')[0].split('-');
+						
+						var $citation = '';
+						if ($topicParams[0].length && $topicParams[3].length && $authorId.length)
+						{
+							var result = $topicParams[0] + ',' + $topicParams[3] + ',' + $authorId;
+							$citation = '[quotemsg=' + result + ']' + text + '[/quotemsg]';
+						}
+						else
+						{
+							$citation = '[quote]' + text + '[/quote]';
+						}
+					
+						$context.html(
+							$('<a href="#">').html("Citer le texte s&eacute;lectionn&eacute;").on("click", function(linkEvent) {
+								$textarea.trigger("focus").val($textarea.trigger("focus").val() + $citation);
+								window.scrollTo(0, $textarea.offset().top);
+								$context.hide();
+								return false;
+							}
+						)).on("show", function(ev, oEv){
+							var $element = $(this);
+							$element.show().css({
+								"left": "0px",
+								"right": ""
+							});
+							
+							$element.css({
+								"top": (oEv.pageY + 10) + "px",
+								"left": (oEv.pageX + 10) + "px"
+							});
+							
+							var w = $element.outerWidth();
+							if (oEv.pageX + 20 + w > window.innerWidth) {
 								$element.css({
-									"top": (oEv.pageY + 10) + "px",
-									"left": (oEv.pageX + 10) + "px"
+									"right": "10px",
+									"left": ""
 								});
-								
-								var w = $element.outerWidth();
-								if (oEv.pageX + 20 + w > window.innerWidth) {
-									$element.css({
-										"right": "10px",
-										"left": ""
-									});
-								}
-							}).trigger("show", documentEvent);
-							return false;
+							}
+						}).trigger("show", documentEvent);
+						return false;
 					}
 				});
 			}
@@ -105,16 +121,17 @@ function applyPfPlus(values) {
 	if (values.displayYoutube) {
 		$("a[href*='youtu.be'], a[href*='youtube.com']").each(function(index) {
 			var $a = $(this);
-			
-			var idVideo = null;
-			if ($a.attr('uri:domain') == 'youtube.com') {
-				idVideo = URI.parseQuery($a.attr('uri:query')).v;
-			} else {
-				idVideo = $a.attr('uri:filename');
-			}
-			
-			if (idVideo) {
-				$a.replaceWith('<iframe width="560" height="315" src="//www.youtube.com/embed/' + idVideo + '" frameborder="0" allowfullscreen></iframe>');
+			if ($a.parents('.signature').length == 0) {
+				var idVideo = null;
+				if ($a.attr('uri:domain') == 'youtube.com') {
+					idVideo = URI.parseQuery($a.attr('uri:query')).v;
+				} else {
+					idVideo = $a.attr('uri:filename');
+				}
+				
+				if (idVideo) {
+					$a.replaceWith('<iframe width="560" height="315" src="//www.youtube.com/embed/' + idVideo + '" frameborder="0" allowfullscreen></iframe>');
+				}
 			}
 		});
 	}
@@ -123,11 +140,12 @@ function applyPfPlus(values) {
 	if (values.displayDailymotion) {
 		$("a[href*='dai.ly'], a[href*='dailymotion.com/video/']").each(function(index) {
 			var $a = $(this);
-			
-			var idVideo = $a.attr('uri:filename').split('_')[0];
-			
-			if (idVideo) {
-				$a.replaceWith('<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/' + idVideo + '" allowfullscreen></iframe>');
+			if ($a.parents('.signature').length == 0) {
+				var idVideo = $a.attr('uri:filename').split('_')[0];
+				
+				if (idVideo) {
+					$a.replaceWith('<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/' + idVideo + '" allowfullscreen></iframe>');
+				}
 			}
 		});
 	}
@@ -136,11 +154,13 @@ function applyPfPlus(values) {
 	if (values.displayTwitter) {
 		$("a[href*='twitter.com']").each(function( listIndex ) {
 			var $tweet = $(this);
-			var href = $tweet.attr('href');
-			
-			if (href.indexOf('status') > 0) {// Lien au format classique https://twitter.com/jack/status/20
-				$tweet.replaceWith('<iframe border=0 frameborder=0 height=250 width=550 src="https://twitframe.com/show?url=' + href + '"></iframe><br />');
-				return;
+			if ($tweet.parents('.signature').length == 0) {
+				var href = $tweet.attr('href');
+				
+				if (href.indexOf('status') > 0) {// Lien au format classique https://twitter.com/jack/status/20
+					$tweet.replaceWith('<iframe border=0 frameborder=0 height=250 width=550 src="https://twitframe.com/show?url=' + href + '"></iframe><br />');
+					return;
+				}
 			}
 		});
 
@@ -176,19 +196,60 @@ function applyPfPlus(values) {
 		});
 	}
 	
-	if (true) {
-		$(".reponserapide").addClass('pf-plus-reponserapide').focus(function() {
-			$("#content_form").animate({
-				height: "+=100"
-			}, 500, function() {
-				 // Animation complete
-			});
-		}).focusout(function() {
-			$("#content_form").animate({
-				height: "-=100"
-			}, 500, function() {
-				 // Animation complete
-			});
+	if (true && $('.reponserapide').length > 0) {
+		// Injection du JS du commentaire
+		$.getScript("/static/compressed/editPost.js?v=11102781422");
+		
+		// nettoyage
+		$("#md_fast_search").children("b, br").remove();
+		
+		// Ajout de l'input pour l'aperçu
+		var $newInput = $('<input type="button" accesskey="a" value="Aperçu" />').on("click", function(){
+			$('#apercu').css({ display: ''});
+			$('#apercu_contenu').val($("#content_form").val());
+			$('#apercu_form').submit();
 		});
+		
+		// Récupération des inputs
+		var $inputs = $("#md_fast_search input").add($newInput).detach();
+		
+		// Récupération du textarea
+		var $reponserapide = $(".reponserapide").detach();
+		
+		// Injection de notre interface
+		$("#addComment").after('<div class="pf-plus-wrap-reponserapide"><div class="pf-plus-buttons-reponserapide"><img tag="b" title="Mettre en gras le texte. Syntaxe : [b]texte[/b]" alt="[b]" src="/static/icones/message/bold.gif" /><img tag="i" title="Mettre en italique le texte. Syntaxe : [i]texte[/i]" alt="[i]" src="/static/icones/message/italic.gif" /><img tag="u" title="Souligner le texte. Syntaxe : [u]texte[/u]" alt="[u]" src="/static/icones/message/underline.gif" /><img tag="strike" title="Barrer le texte. Syntaxe : [strike]texte[/strike]" alt="[strike]" src="/static/icones/message/strike.gif" /><img tag="spoiler" title="Masquer le texte. Syntaxe : [spoiler]texte[/spoiler]" alt="[spoiler]" src="/static/icones/message/spoiler.gif" /><img tag="fixed" title="Ecrire avec une police de taille fixe. Syntaxe : [fixed]texte[/fixed]" alt="[fixed]" src="/static/icones/message/fixe.gif" /><img tag="cpp" title="Insérer du code C/C++. Syntaxe : [cpp]texte[/cpp]" alt="[cpp]" src="/static/icones/message/c.gif" /><img tag="url" title="Insérer une URL. Syntaxe : [url]texte[/url]" alt="[url]" src="/static/icones/message/url.gif" /><img tag="email" title="Insérer une adresse email. Syntaxe : [email]email@hostname.com[/email]" alt="[email]" src="/static/icones/message/email.gif" /><img tag="img" title="Insérer une image. Syntaxe : [img]http://www.exemple.com/exemple.jpg[/img]" alt="[img]" src="/static/icones/message/image.gif" /><img tag="*" title="Insérer une puce. Syntaxe : [*]texte" alt="[*]" src="/static/icones/message/puce.gif" /></div></div>');
+		$(".pf-plus-wrap-reponserapide").append($reponserapide);
+		
+		// Injection du JS
+		$(".pf-plus-buttons-reponserapide img").on("click", function(){
+			var tag = $(this).attr("tag");
+			var open = '[' + tag + ']';
+			var close = '';
+			
+			if (tag != "*") {
+				close = '[/' + tag + ']';
+			}
+			
+			TAinsert(open, close);
+		});
+		
+		// Injection des inputs
+		$(".pf-plus-wrap-reponserapide").after($("<div></div>").append($inputs));
+		
+		// Amélioration affichage
+		$(".reponserapide").addClass('pf-plus-reponserapide');
+			
+		// Injection de l'aperçu
+		$("#md_fast_search").append('<div id="apercu" style="display:none"><table><tr class="reponse">'+
+										'<th class="repCase1">Aperçu</th>'+
+										'<th class="repCase2"><iframe name="apercu_frame" id="apercu_frame" style="border:0px" src=""></iframe></th>'+
+									'</tr></table></div>');
+									
+		$("#md_fast_search").append('<form action="/apercu.php" target="apercu_frame" method="post" name="apercu_form" id="apercu_form">'+
+										'<input type="hidden" name="hash_check" value="3175f08ad575ef5790686004db5c0635">'+
+										'<input type="hidden" name="apercu_contenu" id="apercu_contenu" value="">'+
+										'<input type="hidden" name="config" value="pf.inc">'+
+									'</form>');							
+									
 	}
 }
